6051d532679dcaeb4579b00fc0c8fdbdb6275c72
Yonaim <yonaimyo@gmail.com>
2026-01-02 19:24:38 +0900

    Fix: remove my header including

--- test/include/test_namespace.h
+++ test/include/test_namespace.h
@@ -26,46 +26,7 @@ namespace ft = std;
     // Use custom ft headers
     // -----------------------------
 
-    // ========== algorithm ==========
-    #include "algorithm/equal.h"
-    #include "algorithm/lexicographical_compare.h"
-
-    // ========== allocator ==========
-    #include "allocator/allocator.h"
-
-    // ========== container ==========
-    #include "container/map.h"
-    #include "container/stack.h"
-    #include "container/vector.h"
-
-    // ========== functional ==========
-    #include "functional/binary_function.h"
-    #include "functional/equal_to.h"
-    #include "functional/less.h"
-    #include "functional/select1st.h"
-    #include "functional/unary_function.h"
-
-    // ========== iterator ==========
-    #include "iterator/distance.h"
-    #include "iterator/iterator_tags.h"
-    #include "iterator/iterator_traits.h"
-    #include "iterator/reverse_iterator.h"
-
-    // ========== tree (RB-tree internals) ==========
-    #include "tree/rb_tree.h"
-    #include "tree/rb_tree_impl.h"
-    #include "tree/rb_tree_iterator.h"
-    #include "tree/rb_tree_node.h"
-
-    // ========== type_traits ==========
-    #include "type_traits/bool_type.h"
-    #include "type_traits/enable_if.h"
-    #include "type_traits/is_floating_point.h"
-    #include "type_traits/is_integral.h"
-    #include "type_traits/remove_cv.h"
-
-    // ========== utility ==========
-    #include "utility/pair.h"
+    // TODO : include your implemented headers
 
 #endif // STD_MODE
 
